09a68c2b396eea7b2540978230b8b0c32fbf3126
diegoripollramirez <diegoripollramirez@gmail.com>
2025-01-27 17:53:37 +0100

    confirmation pending api restart

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,15 @@
 import bodyParser from "body-parser";
 import express, { Application } from "express";
 import authRoute from "./routes/authRoute";
+import cityRouter from "./routes/cityRoute";
 
 const app: Application = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Routes
 app.use("/api", authRoute);
+app.use("/", cityRouter); //Just 1 route, we need the cityRoute File?
+
 
 export default app;
