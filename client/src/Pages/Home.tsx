33d2fe8b3122bc34ef949f28ba6b2237f4c1c4ac
nadia-amouei <nadiaamouei09@gmail.com>
2025-01-27 16:37:09 +0100

    add navbar and also home page

---
+++ client/src/Pages/Home.tsx
@@ -0,0 +1,19 @@
+import Navbar from "../Components/Navbar";
+
+
+
+const Home: React.FC = () => {
+
+  return (
+    <>
+      <Navbar>
+
+      </Navbar> 
+      <div className="map-container">
+          map should be shown here
+      </div>
+    </>
+  );
+};
+
+export default Home;
