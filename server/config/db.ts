4b7d8e6deff399097c9634b99b601c1432e406f9
Andreas Reinelt <andreasreinelt@me.com>
2025-01-27 10:41:58 +0100

    FEAT: npm run start works now on server and client npm run develop works on server and client as well and sets .env to develop, builds and runs TypeScript Server nodemon is installed and npm run nodemon starts the server with .env develop and nodemon

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
+process.env.NODE_ENV == 'develop'
+  ? require('dotenv').config({ path: '.env.development' })
+  : require('dotenv').config({ path: '.env.production' })
 
 export const connectDB = async () => {
   try {
-    const HOST = process.env.Host || '127.0.0.1'
-    const DATABASE = process.env.DATABASE || 'placay'
+    const HOST = process.env.DB_HOST || '127.0.0.1'
+    const DATABASE = process.env.DB_NAME || 'placay'
     const conn = await mongoose.connect(`mongodb://${HOST}:27017/${DATABASE}`);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
