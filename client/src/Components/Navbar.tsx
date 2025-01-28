b24ef1d36557ceb2f4c81338504bc73f8cb9a030
Andreas Reinelt <andreasreinelt@me.com>
2025-01-28 18:39:40 +0100

    FIX: In NavBar the div was cursor-pointer, now its just the a links

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -1,28 +1,28 @@
 const Navbar: React.FC = () => {
-  const formatDate = (date: Date): string => {
-    const options: Intl.DateTimeFormatOptions = { 
-      weekday: 'short', 
-      month: 'short', 
-      day: 'numeric', 
-      year: 'numeric' 
+  const formatDate = (date: Date, locale: string = navigator.language || 'en-US'): string => {
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: 'short',
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric'
     };
-    return date.toLocaleDateString('en-US', options).replace(',', '');
+    return date.toLocaleDateString(locale, options).replace(',', '');
   };
 
   const currentDate = new Date();
 
   return (
     <div className="bg-white border-b border-gray-200 shadow-sm flex flex-row items-center ">
-      <img src="/asserts/images/favicon.png" alt="placay logo" className="w-15 mx-3 "/>
+      <img src="/asserts/images/favicon.png" alt="placay logo" className="w-15 mx-3 " />
 
       <div className="border-r border-l border-gray-300 px-2 h-15 flex items-center whitespace-nowrap text-black">{formatDate(currentDate)} </div>
 
-      <div className="flex flex-row gap-3 justify-start w-full cursor-pointer ">
-        <a className="px-4 hover:text-blue-400 text-gray-700">Map</a> 
-        <a className="px-4 hover:text-blue-400 text-gray-700">Tours</a>
-        <a href="/login" className="ml-auto px-4 hover:text-blue-400 text-gray-700">Login</a>
+      <div className="flex flex-row gap-3 justify-start w-full">
+        <a className="px-4 hover:text-blue-400 text-gray-700 cursor-pointer">Map</a>
+        <a className="px-4 hover:text-blue-400 text-gray-700 cursor-pointer">Tours</a>
+        <a href="/login" className="ml-auto px-4 hover:text-blue-400 text-gray-700 cursor-pointer">Login</a>
       </div>
-      
+
     </div>
   );
 };
