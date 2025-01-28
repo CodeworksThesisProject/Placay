316718b59cf7e5fede14f4b361b7536f1fa515d1
Andreas Reinelt <andreasreinelt@me.com>
2025-01-28 18:54:24 +0100

    FEAT: NavBar and Footer: set default text color to #38436C

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -13,14 +13,14 @@ const Navbar: React.FC = () => {
 
   return (
     <div className="bg-white border-b border-gray-200 shadow-sm flex flex-row items-center ">
-      <img src="/asserts/images/favicon.png" alt="placay logo" className="w-15 mx-3 " />
+      <img src="/asserts/images/placay-just-logo.png" alt="Placay Logo" className="w-15 mx-3 " />
 
-      <div className="border-r border-l border-gray-300 px-2 h-15 flex items-center whitespace-nowrap text-black">{formatDate(currentDate)} </div>
+      <div className="border-r border-l border-gray-300 px-2 h-15 flex items-center whitespace-nowrap text-[#38436C]">{formatDate(currentDate)} </div>
 
       <div className="flex flex-row gap-3 justify-start w-full">
-        <a className="px-4 hover:text-blue-400 text-gray-700 cursor-pointer">Map</a>
-        <a className="px-4 hover:text-blue-400 text-gray-700 cursor-pointer">Tours</a>
-        <a href="/login" className="ml-auto px-4 hover:text-blue-400 text-gray-700 cursor-pointer">Login</a>
+        <a className="px-4 hover:text-blue-400 text-[#38436C] cursor-pointer">Map</a>
+        <a className="px-4 hover:text-blue-400 text-[#38436C] cursor-pointer">Tours</a>
+        <a href="/login" className="ml-auto px-4 hover:text-blue-400 text-[#38436C] cursor-pointer">Login</a>
       </div>
 
     </div>
