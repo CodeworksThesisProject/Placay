4bb44875e78b6beb6932eaf5b470932de48d41dd
diegoripollramirez <diegoripollramirez@gmail.com>
2025-01-29 16:38:24 +0100

    removed unused import

--- client/src/Components/SearchField.tsx
+++ client/src/Components/SearchField.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { APIProvider, useMapsLibrary } from '@vis.gl/react-google-maps';
 //npm install "@vis.gl/react-google-maps"
 //npm i -D @types/google.maps
