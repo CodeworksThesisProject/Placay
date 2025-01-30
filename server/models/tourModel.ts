cefc503ab0f6b99823564f4156f4164f0e72f5e0
diegoripollramirez <diegoripollramirez@gmail.com>
2025-01-30 13:07:41 +0100

    tourController

--- server/models/tourModel.ts
+++ server/models/tourModel.ts
@@ -3,10 +3,9 @@ import mongoose from 'mongoose';
 const tourSchema = new mongoose.Schema({
   title: { type: String, required: true },
   city: { type: String, required: true },
-  country: { type: Number, required: true },
+  country: { type: String, required: true },
   duration: { type: String, required: true },
   locations: { type: mongoose.Schema.Types.Mixed },
 });
-
 
 export const Tour = mongoose.model('Tour', tourSchema);
