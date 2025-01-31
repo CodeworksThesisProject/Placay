0ea449c1d99645b0ccd5c4453aa8d65ab8f6170d
nadia-amouei <nadiaamouei09@gmail.com>
2025-01-31 10:42:28 +0100

    fix bug footer style

--- client/src/Components/footer.tsx
+++ client/src/Components/footer.tsx
@@ -8,7 +8,7 @@ function Footer(): JSX.Element {
   }
 
   return (
-    <footer className="fixed bottom-0 left-0 w-full bg-gray-100 text-sm text-gray-700 border-t border-gray-300 py-4 flex items-center justify-between px-10">
+    <footer className="z-[9999] fixed bottom-0 left-0 w-full bg-gray-100 text-sm text-gray-700 border-t border-gray-300 py-4 flex items-center justify-between px-10">
       {/* Name and Year */}
       <div className="absolute left-1/2 transform -translate-x-1/2 text-[#38436C]">
         © 2025 Placay
