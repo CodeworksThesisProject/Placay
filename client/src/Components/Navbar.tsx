04b1251a2ad0a78e239fec0d27ba989279d20769
nadia-amouei <nadiaamouei09@gmail.com>
2025-01-31 11:05:56 +0100

    fix bug header style

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -81,7 +81,7 @@ const Navbar: React.FC = () => {
     <div className="bg-white border-b border-gray-200 shadow-sm flex flex-row items-center ">
       <img src="/asserts/images/placay-just-logo.png" alt="Placay Logo" className="w-15 mx-3 cursor-pointer" onClick={() => navigate('/')} />
       <div className="border-r border-l border-gray-300 px-2 h-15 flex items-center whitespace-nowrap text-[#38436C]">{new Date().toLocaleDateString()}</div>
-      <div className="flex flex-row gap-3 justify-start w-full">
+      <div className="flex flex-row gap-3 justify-start items-center w-full">
         <a className="px-4 hover:text-blue-400 text-[#38436C] cursor-pointer" onClick={() => navigate('/')}>Map</a>
         <a className="px-4 hover:text-blue-400 text-[#38436C] cursor-pointer" onClick={() => navigate('/tours')}>Tours</a>
         {/* if user is login then show username btn which redirect to profile page */}
