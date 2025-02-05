634d4f44620b8b0624c37e71fd074f805b7f1de7
Andreas Reinelt <andreasreinelt@me.com>
2025-02-05 12:12:11 +0100

    FEAT: Navbar with Profile Image when logged in, Placay Logo, better handeling on smaller screens

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -52,13 +52,19 @@ const Navbar: React.FC = () => {
           className="w-12 cursor-pointer"
           onClick={() => navigate('/')}
         />
-        <div className="text-[#38436C]">{formatDate(new Date())}</div>
-        <button className="px-4 py-2 text-[#38436C] hover:text-blue-400 cursor-pointer" onClick={() => navigate('/')}>Map</button>
-        <button className="px-4 py-2 text-[#38436C] hover:text-blue-400 cursor-pointer" onClick={() => navigate('/tours')}>Tours</button>
+        <img
+          src="/asserts/images/placay.svg"
+          alt="Placay Logo"
+          className="w-40 cursor-pointer"
+          onClick={() => navigate('/')}
+        />
+        <div className="hidden md:block text-[#38436C]">{formatDate(new Date())}</div>
+        <button className="hidden md:block px-4 py-2 text-[#38436C] hover:text-blue-400 cursor-pointer" onClick={() => navigate('/')}>Map</button>
+        <button className="hidden md:block px-4 py-2 text-[#38436C] hover:text-blue-400 cursor-pointer" onClick={() => navigate('/tours')}>Tours</button>
         {/* {isAuthenticated && user?.role === 'admin' ? (
           <button className="px-4 py-2 text-[#38436C] hover:text-blue-400 cursor-pointer" onClick={() => navigate('/dashboard')}>Dashboard</button>
         ) : ("")} */}
-    </div>
+      </div>
 
       <div className="md:hidden">
         <button className="px-4 py-2 text-[#38436C] hover:text-blue-400 cursor-pointer" onClick={() => setIsMenuOpen(!isMenuOpen)}>
@@ -66,11 +72,20 @@ const Navbar: React.FC = () => {
         </button>
       </div>
 
-      <div className={`absolute top-14 left-0 w-full bg-white shadow-md p-3 flex flex-col md:hidden ${isMenuOpen ? 'block' : 'hidden'}`}>
+      <div className={`absolute top-14 left-0 w-full bg-white shadow-md p-3 flex flex-col md:hidden ${isMenuOpen ? 'block' : 'hidden'} z-51`}>
+        <button className="px-4 py-2 text-[#38436C] hover:text-blue-400 cursor-pointer" onClick={() => navigate('/profile')}>View Profile</button>
         <button className="px-4 py-2 text-[#38436C] hover:text-blue-400 cursor-pointer" onClick={() => navigate('/map')}>Map</button>
-        <button className="px-4 py-2 text-[#38436C] hover:text-blue-400 cursor-pointer" onClick={() => navigate('/tours')}>Tours</button>
+        <button className="px-4 py-2 text-[#38436C] hover:text-blue-400 cursor-pointer" onClick={() => navigate('/mytours')}>My Tours</button>
+        <button className="px-4 py-2 text-[#38436C] hover:text-blue-400 cursor-pointer" onClick={() => navigate('/mylocations')}>My Locations</button>
+        {isAuthenticated && user?.role === "admin" ? (
+          <div>
+            <button className="px-4 py-2 text-[#38436C] hover:bg-orange-100 w-full cursor-pointer" onClick={() => { navigate('/dashboard'); closeDropdown(); }}>Dashboard</button>
+            <button className="px-4 py-2 text-[#38436C] hover:bg-orange-100 w-full cursor-pointer" onClick={() => { navigate('/authTest'); closeDropdown(); }}>AuthTest</button>
+            <button className="px-4 py-2 text-[#38436C] hover:bg-orange-100 w-full cursor-pointer" onClick={() => { navigate('/function-test'); closeDropdown(); }}>Function Test</button>
+          </div>
+        ) : ("")}
         {isAuthenticated ? (
-          <button className="px-4 py-2 text-[#38436C] hover:text-blue-400 cursor-pointer" onClick={handleLogout}>Logout</button>
+          <button className="px-4 py-2 text-[#38436C] hover:bg-red-100 cursor-pointer" onClick={handleLogout}>Logout</button>
         ) : (
           <button className="px-4 py-2 text-[#38436C] hover:text-blue-400 cursor-pointer" onClick={() => navigate('/login')}>Login</button>
         )}
@@ -78,9 +93,14 @@ const Navbar: React.FC = () => {
 
       <div className="relative hidden md:flex items-center">
         {isAuthenticated ? (
-          <div className="relative ml-auto">
-            <button className="px-4 py-2 text-[#38436C] hover:text-blue-400 cursor-pointer" onClick={toggleDropdown}>{user ? `${user.name} ` : ""}Profile</button>
-            
+          <div className="relative ml-auto flex items-center gap-2 cursor-pointer" onClick={toggleDropdown} >
+            <button className="px-4 py-2 text-[#38436C] hover:text-blue-400 cursor-pointer" >{user ? `${user.name} ` : ""}Profile</button>
+            <img
+              src={user?.profileImage || "/asserts/images/profilePictures/default-profile-picture.png"}
+              alt="Profile"
+              className="w-10 h-10 rounded-full object-cover"
+            />
+
             {isDropdownOpen && (
               <div className="absolute top-full right-0 mt-2 w-40 bg-white border border-gray-200 rounded shadow-md z-50">
                 <button className="block px-4 py-2 text-[#38436C] hover:bg-gray-100 w-full text-center cursor-pointer" onClick={() => { navigate('/profile'); closeDropdown(); }}>
@@ -94,9 +114,9 @@ const Navbar: React.FC = () => {
                 </button>
                 {isAuthenticated && user?.role === "admin" ? (
                   <div>
-                  <button className="block px-4 py-2 text-[#38436C] hover:bg-orange-100 w-full text-center cursor-pointer" onClick={() => { navigate('/dashboard'); closeDropdown(); }}>Dashboard</button>
-                  <button className="block px-4 py-2 text-[#38436C] hover:bg-orange-100 w-full text-center cursor-pointer" onClick={() => { navigate('/authTest'); closeDropdown(); }}>AuthTest</button>
-                  <button className="block px-4 py-2 text-[#38436C] hover:bg-orange-100 w-full text-center cursor-pointer" onClick={() => { navigate('/function-test'); closeDropdown(); }}>Function Test</button>
+                    <button className="block px-4 py-2 text-[#38436C] hover:bg-orange-100 w-full text-center cursor-pointer" onClick={() => { navigate('/dashboard'); closeDropdown(); }}>Dashboard</button>
+                    <button className="block px-4 py-2 text-[#38436C] hover:bg-orange-100 w-full text-center cursor-pointer" onClick={() => { navigate('/authTest'); closeDropdown(); }}>AuthTest</button>
+                    <button className="block px-4 py-2 text-[#38436C] hover:bg-orange-100 w-full text-center cursor-pointer" onClick={() => { navigate('/function-test'); closeDropdown(); }}>Function Test</button>
                   </div>
                 ) : ("")}
                 <button className="block px-4 py-2 text-[#38436C] hover:bg-red-100 w-full text-center cursor-pointer" onClick={() => { handleLogout(); closeDropdown(); }}>
@@ -104,7 +124,7 @@ const Navbar: React.FC = () => {
                 </button>
               </div>
             )}
-           </div>
+          </div>
         ) : (
           <button className="ml-auto px-4 py-2 text-[#38436C] hover:text-blue-400 cursor-pointer" onClick={() => navigate('/login')}>Login</button>
         )}
