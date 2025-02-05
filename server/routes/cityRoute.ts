c573d20922f492b8c6bd67248accb603ecf214a1
diegoripollramirez <diegoripollramirez@gmail.com>
2025-02-05 17:23:07 +0100

    .

--- server/routes/cityRoute.ts
+++ server/routes/cityRoute.ts
@@ -3,10 +3,9 @@ const router = express.Router();
 import { getPointsOfInterest, getDetails } from "../controllers/pointsOfInterestController";
 import { autocomplete, getCoordinates } from "../controllers/searchButtonController";
 
-router.get('/autocomplete/:input',autocomplete)
-router.get('/details/:placeId',getCoordinates)
 router.post('/:cityName', getPointsOfInterest);
 router.get('/:point_id', getDetails)
-
+router.get('/autocomplete/:input',autocomplete)
+router.get('/details/:placeId',getCoordinates)
 
 export default router;
